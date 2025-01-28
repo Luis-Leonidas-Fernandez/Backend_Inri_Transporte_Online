b8a182f620cde3cfcb7ac2fcdf585aa7cc8f6667
Tobias Nuñez <tobias.nicolas001@gmail.com>
2025-01-27 21:44:15 -0300

    dbConnection mejorada
    - Crear conexión al puerto por defecto de MongoDB si no se configura la variable de entorno.
    - Actualizar el proyecto a ES Modules.
    - Añadir logs para rastrear errores.

--- database/config.js
+++ database/config.js
@@ -1,22 +1,15 @@
-const  mongoose = require('mongoose');
+import mongoose from "mongoose";
 
-
-const dbConnection = async() => {
-
-    try {
-        mongoose.set('strictQuery', true);
-        await mongoose.connect( process.env.DB_URL,             
-        );
-
-       
-        
-
-    } catch (error) {       
-        throw new Error('Error en la base de datos - Hable con el admin');
-    }
-
-}
-
-module.exports = {
-    dbConnection
-}
+/**
+ * Establece la conexión con la base de datos.
+ * Utiliza la URL definida en la variable de entorno `DB_URL` o, en su defecto, conecta a MongoDB en localhost (puerto 27017).
+ */
+export const dbConnection = async () => {
+  try {
+    mongoose.set("strictQuery", true);
+    await mongoose.connect(process.env.DB_URL || "mongodb://localhost:27017");
+    console.log("Base de datos conectada correctamente.");
+  } catch (error) {
+    console.log("Error al conectar la base de datos: ", error);
+  }
+};
