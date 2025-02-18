db651fe673b7e3c5ee1062c0bcb068bb90b0c54b
Tobias Nuñez <tobias.nicolas001@gmail.com>
2025-02-18 14:27:50 -0300

    BidsSchema creado
    - Se creó el esquema de bids.

---
+++ models/bids.js
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+
+const BidsSchema = Schema(
+  {
+    idConductor: {
+      type: Schema.Types.ObjectId,
+      ref: "Conductor",
+      required: true,
+    },
+    idOrder: {
+      type: Schema.Types.ObjectId,
+      ref: "Conductor",
+      required: true,
+    },
+    oferta: {
+      valor: {
+        type: Number,
+        required: true,
+      },
+      moneda: {
+        type: String,
+        required: true,
+      },
+    },
+    comentario: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+BidsSchema.method("toJSON", function () {
+  const { __v, _id, password, ...object } = this.toObject();
+  object.uid = _id;
+  return object;
+});
+
+const Bid = model("Bid", BidsSchema);
+
+export default Bid;
