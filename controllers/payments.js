eac70e46d5a355eb3016ba7a74e1164b720575df
Tobias Nuñez <tobias.nicolas001@gmail.com>
2025-04-11 17:55:39 -0300

    feat: conectar mp
    - Se creó una función para conectar mercado pago del conductor.
    - Se creó una función que devuelve el accesstoken del conductor.
    - Se añadió a la ruta payments.

--- controllers/payments.js
+++ controllers/payments.js
@@ -1,8 +1,63 @@
 import { MercadoPagoConfig, Preference } from "mercadopago";
-import dotenv from 'dotenv'
+import fetch from "node-fetch";
+import dotenv from "dotenv";
 
-dotenv.config()
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN
+dotenv.config();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = "https://www.tobiasnicolasn.com/";
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+
+/** Redirige al usuario/conductor a Mercado Pago para que autorice actuar en su nombre (Crear pagos). Es parte del flujo OAuth 2.0 con Authorization Code. */
+export const connectMercado = (_req, res) => {
+  try {
+    const redirect = `https://auth.mercadopago.com/authorization?client_id=${CLIENT_ID}&response_type=code&platform_id=mp&state=AUTORIZACIONCLIENTE&redirect_uri=${REDIRECT_URI}`;
+    console.log(redirect);
+    res.status(200).json({ redirect });
+  } catch (error) {
+    res.status(400).json({ error: error });
+    console.log("[payments.connectMercado] Se ha producido un error", error);
+  }
+};
+
+/** Función que se encarga de recibir el código de autorización y obtener el access_token y refresh_token */
+export const getAccessToken = async (req, res) => {
+  const { code } = req.query;
+
+  const params = new URLSearchParams();
+  params.append("grant_type", "authorization_code");
+  params.append("client_id", CLIENT_ID);
+  params.append("client_secret", CLIENT_SECRET);
+  params.append("code", code);
+  params.append("redirect_uri", REDIRECT_URI);
+
+  try {
+    const response = await fetch("https://api.mercadopago.com/oauth/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: params,
+    });
+
+    const data = await response.json();
+
+    // Utilizamos el access_token y refresh_token para realizar acciones en nombre del usuario
+    const { access_token, refresh_token, user_id } = data;
+
+    console.log(
+      "[payments.getAccessToken] Se ha obtenido el access_token:",
+      access_token,
+      "del usuario:",
+      user_id
+    );
+    res.status(200).json({ access_token, user_id });
+  } catch (error) {
+    console.error("[payments.getAccessToken] Se ha producido un error:", error);
+    res.status(500).json({ error: error });
+  }
+};
 
 const client = new MercadoPagoConfig({
   accessToken: ACCESS_TOKEN,
@@ -18,8 +73,8 @@ export async function createPreference(_req, res) {
       body: {
         items: [
           {
-            id: "generico",
-            title: "prueba",
+            id: "1",
+            title: "Envio - Duelos",
             quantity: 1,
             unit_price: 20000,
             currency_id: "ARS",
@@ -30,7 +85,7 @@ export async function createPreference(_req, res) {
     console.log(result.sandbox_init_point);
     res.status(200).json({ urlPago: result.sandbox_init_point });
   } catch (err) {
-    res.status(500).json({ error: "No se pudo crear el pago" });
+    res.status(400).json({ error: "No se pudo crear el pago" });
     console.log("[payments.createPreference] Se ha producido un error:", err);
   }
 }
--- routes/payments.js
+++ routes/payments.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
-import { createPreference } from "../controllers/payments.js";
+import {
+  connectMercado,
+  createPreference,
+  getAccessToken,
+} from "../controllers/payments.js";
 import { validarJWT } from "../middlewares/validar-jwt.js";
 
 const router = Router();
 
 // path: /api/payments
 router.post("/payments", validarJWT, createPreference);
+router.get("/payments/connect", validarJWT, connectMercado);
+router.get("/payments/accessToken", validarJWT, getAccessToken);
 
 export default router;
+ 
