da07bfaa9ec7de924e858a2590bfe7b982ca609d
Tobias Nuñez <tobias.nicolas001@gmail.com>
2025-04-16 11:14:36 -0300

    feat: PaymentCredentialsSchema
    - Se creó el schema para guardar en la DB los datos necesarios para la manipulación de mercadopago.

---
+++ models/paymentCredential.js
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+
+const PaymentCredentialsSchema = Schema(
+  {
+    idConductor: {
+      type: Schema.Types.ObjectId,
+      ref: "Conductor",
+      required: true,
+    },
+    accessToken: {
+      type: String,
+      required: true,
+    },
+    provider: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+PaymentCredentialsSchema.method("toJSON", function () {
+  const { __v, ...object } = this.toObject();
+  return object;
+});
+
+const PaymentCredential = model("PaymentCredential", PaymentCredentialsSchema);
+
+export default PaymentCredential;
